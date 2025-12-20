19534198ee858703298362da4ca782eb98562ab9
jellyfish0316 <bryan930316@gmail.com>
2025-12-20 17:17:40 +0800

    finish lab3

---
+++ lab3/LAB3/3/3_2/My_Kernel.c
@@ -0,0 +1,56 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <asm/current.h>
+
+#define procfs_name "Mythread_info"
+#define BUFSIZE  1024
+char buf[BUFSIZE]; //kernel buffer
+
+static ssize_t Mywrite(struct file *fileptr, const char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /*Your code here*/
+    copy_from_user(buf, ubuf, buffer_len);
+
+    sprintf(buf + buffer_len, "PID: %d, TID: %d, time: %lld\n", 
+                    current->tgid, current->pid, current->utime/100/1000);
+
+    return buffer_len;
+    /****************/
+}
+
+
+static ssize_t Myread(struct file *fileptr, char __user *ubuf, size_t buffer_len, loff_t *offset){
+    /*Your code here*/
+    int len;
+    len = strlen(buf);
+
+    if(*offset > 0){
+        return 0;
+    }
+    copy_to_user(ubuf, buf, len);
+    *offset += len;
+    return len;
+    /****************/
+}
+
+static struct proc_ops Myops = {
+    .proc_read = Myread,
+    .proc_write = Mywrite,
+};
+
+static int My_Kernel_Init(void){
+    proc_create(procfs_name, 0644, NULL, &Myops);   
+    pr_info("My kernel says Hi");
+    return 0;
+}
+
+static void My_Kernel_Exit(void){
+    pr_info("My kernel says GOODBYE");
+}
+
+module_init(My_Kernel_Init);
+module_exit(My_Kernel_Exit);
+
+MODULE_LICENSE("GPL");
